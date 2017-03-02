4dd40564a0f1f88be0e5a70b3f3a885ab166a080
Yanuar Nurcahyo <yanuar@microad.co.id>
2017-03-02 10:04:02 +0700

    main configurations

--- backend/config/main.php
+++ backend/config/main.php
@@ -5,7 +5,7 @@ $params = array_merge(
     require(__DIR__ . '/params.php'),
     require(__DIR__ . '/params-local.php')
 );
-$projectName = $params['project']['firstname'] . $params['project']['lastname'];
+$projectName = 'backend' . $params['project']['firstname'] . $params['project']['lastname'];
 
 return [
     'id' => 'app-backend',
